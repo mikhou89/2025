31b624fd606b6f17d7ddf594091db070976e6dde
mikhou89 <buisnessamirdabou@gmail.com>
2025-04-28 21:20:25 +0200

    HellV2.py

--- script1.py
+++ script1.py
@@ -2,7 +2,7 @@
 
 def greet_user():
     name = input("Enter your name: ")
-    print(f"Hello, {name}! Welcome to the world of Python.")
+    print(f"Hello, {name}! Welcome to the world of Python ya Amir.")
 
 if __name__ == "__main__":
     greet_user()
